28a43806ef461e5bb72cf8fc36723aa4ab4c2d31
Divya <divyakoslia@gmail.com>
2025-09-18 17:55:48 +0530

    Assignmnent 2 (python)

--- Assignment2.py
+++ Assignment2.py
@@ -14,11 +14,11 @@ print(f"Hello, {name}")
 #3 swapping numbers 
 x = int(input("Enter value of x : "))
 y = int(input("Enter value of y : "))
-x = x+y
-y = x-y
-x = x-y
-print("after swapping x is : ",x)
-print("after swapping y is : ",y)
+temp = x
+x = y
+y = temp
+print("value of x : ",x)
+print("value of y : ",y)
 
 
 #4 even oddd number 
@@ -51,14 +51,12 @@ else :
 
 
 #7 smallest of three numbers 
-num_1 = input("enter 1st number: ")
-num_2 = input("enter 2nd number: ")
-num_3 = input("enter 3rd number: ")
-if num_1<num_2 :
-    if num_1<num_3 :
+num_1 = int(input("enter 1st number: "))
+num_2 = int(input("enter 2nd number: "))
+num_3 = int(input("enter 3rd number: "))
+if num_1<num_2 and num_1<num_3 :
         print("smallest number is ",num_1)
-elif num_2<num_1 :
-    if num_2<num_3 :
+elif num_2<num_3 :
         print("smallest number is ",num_2)
 else :
     print("smallest number is ",num_3)
@@ -67,12 +65,16 @@ else :
 #8 square and cube of a number 
 x = int(input("enter number: "))
 print("square is ",x*x)
-print("cube is ",x*x*x)
+print("cube is ",x**3)
 
 
 #9 sum of digits of a given number 
 b = int(input("enter number: "))
-output = sum(int(x) for x in str(b))
+output = 0
+while b>0 :
+    last_digit = b%10
+    output += last_digit
+    b //= 10
 print("sum of digits is : ",output)
 
 
@@ -84,19 +86,21 @@ for i in range(1,11) :
 #11 factorial of a number 
 num = int(input("enter a number : "))
 factorial = 1
-if num<0 :
-    print("no factorial exist ")
-elif num==0 :
-    print("factorial is 1")
-else :
-    for i in range (1,num+1):
+for i in range (1,num+1):
         factorial = factorial*i
 print( "factorial is ",factorial)
 
 
 #12 reverse 
-d = input("enter number : ")
-print(d[::-1])
+num = int(input("Enter a number: ")) 
+rev = 0 
+while num > 0: 
+    digit = num % 10 
+    rev = rev * 10 + digit 
+    num //= 10
+print("Reversed number:", rev) 
+         #or
+print(num[::-1])
 
 
 #13 print 10 fibonacci numbers
@@ -125,18 +129,25 @@ print("No. of characters: ",len(text1))
 
 #16 sum of n natural numbers 
 n = int(input("enter value of n : "))
+total = 0 
+for i in range(1, n + 1): 
+    total += i 
+print("Sum:", total) 
+      #or
 print(n*(n+1)/2)
 
 
 #17 multiplication table of a number
+n = int(input("Enter number: ")) 
+for i in range(1, 11): 
+    print(n, "x", i, "=", n * i)
+                #or
 y = int(input("enter table of : "))
 table = []
 for x in range(1,11):
     table.append(x*y)
 print(table)
-
-
-#17 using list comprehension (table)
+                #or
 z = int(input("enter table of :"))
 table_of=[x*z for x in range(1,11)]
 print(table_of)
@@ -147,8 +158,9 @@ str1 = input("enter a string : ")
 str1.lower()
 count = 0
 for i in str1:
-    if i in ["a","e","i","o","u"]:
-        count += 1
+    if i.isalpha() :
+        if i in ["a","e","i","o","u"]:
+         count += 1
 print("vowels in string are : ",count)
 print("consonants are : ",len(str1)-count)
 
@@ -166,19 +178,12 @@ for i in range(1, n + 1):
 
 
 #21 sort a list 
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                # Swap elements
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-# Example usage
-my_list = [5, 2, 9, 1, 5, 6]
-sorted_list = bubble_sort(my_list)
-print("Sorted list:", sorted_list)
-
+nums = [5, 3, 8, 6, 2]
+for i in range(len(nums)): 
+ for j in range(i + 1, len(nums)): 
+  if nums[i] > nums[j]: 
+   nums[i], nums[j] = nums[j], nums[i] 
+print("Sorted:", nums) 
 
 
 #22 maximum and minumum numbers
@@ -188,7 +193,7 @@ min = list1[0]
 for i in list1:
     if i>max:
         max=i
-    elif i<min:
+    if i<min:
         min=i
 print(f"maximum number is {max}")
 print(f"minimum number is {min}")
@@ -205,31 +210,23 @@ print(newlist)
 
 
 #24 anagrams strings
-string1 = input("enter first string: ")
-string2 = input("enter second string: ")
-if len(string1) != len(string2):
-    print("not anagrams")
-else:
-    sorted1 = sorted(string1.lower())
-    sorted2 = sorted(string2.lower())
-    if sorted1 == sorted2 :
+string1 = input("enter first string: ").lower()
+string2 = input("enter second string: ").lower()
+if sorted(string1) == sorted(string2) :
         print("anagrams")
-    else:
+else:
         print("not anagrams ")
 
 
 #25 count frequency of each element 
-def frequency(lst):
-    freq = {}
-    for element in lst:
+lst = [1, 2, 2, 3, 3, 3, 4] 
+freq = {}
+for element in lst:
         if element in freq:
             freq[element] += 1
         else:
             freq[element] = 1
-    return freq
-my_list = [1, 2, 2, 3, 1, 4, 2]
-frequencies = frequency(my_list)
-print("Frequencies:", frequencies)
+print("frequencies : ",freq)
 
 
 #26 second largest number
